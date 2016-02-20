57e58e78e83e20125de3d1b3cdf44e126b23784d
Abdulaziz M. Ghaleb <dr.eng.ghaleb@gmail.com>
2016-02-20 11:36:05 +0300

    Update SocketSrv.py

--- SocketSrv.py
+++ SocketSrv.py
@@ -7,5 +7,5 @@ serverPort=111
 server.bind((serverIP,serverPort))
 server.listen(2)
 (client, ip)=server.accept()
-client.send("How are you network engineers")
-client.close()
+conn.send("How are you network engineers")
+conn.close()
